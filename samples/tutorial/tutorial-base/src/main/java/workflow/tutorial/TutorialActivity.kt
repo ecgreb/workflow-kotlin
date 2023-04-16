5a97bce63c63c122f21034148c636742c8f13202
Chuck Greb <cgreb@fundrise.com>
2023-04-16 11:05:15 -0400

    tutorial part 1

--- samples/tutorial/tutorial-base/src/main/java/workflow/tutorial/TutorialActivity.kt
+++ samples/tutorial/tutorial-base/src/main/java/workflow/tutorial/TutorialActivity.kt
@@ -1,12 +1,38 @@
+@file:OptIn(WorkflowUiExperimentalApi::class)
 package workflow.tutorial
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.squareup.workflow1.ui.ViewRegistry
+import com.squareup.workflow1.ui.WorkflowLayout
+import com.squareup.workflow1.ui.WorkflowUiExperimentalApi
+import com.squareup.workflow1.ui.renderWorkflowIn
+import kotlinx.coroutines.flow.StateFlow
+
+private val viewRegistry = ViewRegistry(WelcomeLayoutRunner)
 
 class TutorialActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.welcome_view)
+    val model: TutorialViewModel by viewModels()
+
+    setContentView(
+      WorkflowLayout(this).apply { start(model.renderings, viewRegistry) }
+    )
+  }
+}
+
+class TutorialViewModel(savedState: SavedStateHandle) : ViewModel() {
+  val renderings: StateFlow<WelcomeScreen> by lazy {
+    renderWorkflowIn(
+      workflow = WelcomeWorkflow,
+      scope = viewModelScope,
+      savedStateHandle = savedState
+    )
   }
 }
---
+++ samples/tutorial/tutorial-base/src/main/java/workflow/tutorial/WelcomeWorkflow.kt
@@ -0,0 +1,43 @@
+package workflow.tutorial
+
+import com.squareup.workflow1.Snapshot
+import com.squareup.workflow1.StatefulWorkflow
+import com.squareup.workflow1.WorkflowAction
+import workflow.tutorial.WelcomeWorkflow.Output
+import workflow.tutorial.WelcomeWorkflow.State
+
+object WelcomeWorkflow : StatefulWorkflow<Unit, State, Output, WelcomeScreen>() {
+
+  data class State(
+    val username: String
+  )
+
+  object Output
+
+  override fun initialState(
+    props: Unit,
+    snapshot: Snapshot?
+  ): State = State(username = "")
+
+  override fun render(
+    renderProps: Unit,
+    renderState: State,
+    context: RenderContext
+  ): WelcomeScreen = WelcomeScreen(
+    username = renderState.username,
+    onUsernameChanged = { context.actionSink.send(onUsernameChanged(it)) },
+    onLoginTapped = {}
+  )
+
+  private fun onUsernameChanged(username: String) =
+    object : WorkflowAction<Unit, State, Nothing>() {
+      override fun Updater.apply() {
+        state = state.copy(username = username + " ")
+      }
+    }
+
+  override fun snapshotState(state: State): Snapshot? = Snapshot.write {
+    TODO("Save state")
+  }
+}
+
